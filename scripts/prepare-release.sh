46f0ad6ae020640210fb36c0686d4fbd55cba317
Maxime Curioni <maxime@ipinfo.io>
2024-08-07 19:02:59 -0600

    New version files cannot be generated or submitted at release time
    The release depends on a submitted tag in `origin`, so must be submitted before-hand.

--- scripts/prepare-release.sh
+++ scripts/prepare-release.sh
@@ -17,6 +17,24 @@ if [ -z "${VSN}" ]; then
     exit 1
 fi
 
+# Generate new versions separately (otherwise we get an empty file)
+cat $ROOT/deb.sh | sed "s/VSN=.*/VSN=${VSN}/" > $ROOT/deb.sh.new
+cat $ROOT/macos.sh | sed "s/VSN=.*/VSN=${VSN}/" > $ROOT/macos.sh.new
+cat $ROOT/windows.ps1 | sed "s/\$VSN =.*/\$VSN = \"${VSN}\"/" > $ROOT/windows.ps1.new
+cat $ROOT/dist/DEBIAN/control | sed "s/Version: .*/Version: ${VSN}/" > $ROOT/dist/DEBIAN/control.new
+
+mv $ROOT/deb.sh.new $ROOT/deb.sh
+mv $ROOT/macos.sh.new $ROOT/macos.sh
+mv $ROOT/windows.ps1.new $ROOT/windows.ps1
+mv $ROOT/dist/DEBIAN/control.new $ROOT/dist/DEBIAN/control
+
+if [ "$(git diff --shortstat)" = " 4 files changed, 4 insertions(+), 4 deletions(-)" ] ; then
+  echo "Success"
+else
+    echo "Platform scripts version update failed" 2>&1
+    git diff | cat 2>&1
+    exit 1
+fi
 
 LATEST_RELEASE_VERSION=$(wget -qO- https://api.github.com/repos/ipinfo/mmdbctl/releases/latest | jq -r ".tag_name")
 LATEST_RELEASE_SEMVER=$(echo ${LATEST_RELEASE_VERSION} | sed 's/mmdbctl-//' )
